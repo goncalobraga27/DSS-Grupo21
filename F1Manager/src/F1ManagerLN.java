e4a07e7ede0ada7e1bf685e2ae1b88c82eda6ec5
goncalobraga27 <a97541@alunos.uminho.pt>
2022-12-28 14:07:45 +0000

    Alterações

--- F1Manager/src/F1ManagerLN.java
+++ F1Manager/src/F1ManagerLN.java
@@ -4,8 +4,13 @@ import java.util.Map;
 public class F1ManagerLN {
     private Map<String,Carro> carros;
     private Map<String,Campeonato> campeonatos;
+    private Map<String,Administrador> administradores;
+    private Map<String,Jogador> jogadores;
+
     public F1ManagerLN(){
         this.carros=CarroDAO.getInstance();
         this.campeonatos=CampeonatoDAO.getInstance();
+        this.administradores=AdministradorDAO.getInstance();
+        this.jogadores=JogadorDAO.getInstance();
     }
 }
